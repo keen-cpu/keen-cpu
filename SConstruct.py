d5dab79ac2cace591d7a3eec39c5c7d5d6209d37
Jacob Koziej <jacobkoziej@gmail.com>
2024-11-17 00:58:19 -0500

    Add `SConstruct.py`

---
+++ SConstruct.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MPL-2.0
+#
+# SConstruct.py
+# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+
+# ruff: noqa: F821
+
+EnsureSConsVersion(4, 7, 0)
+EnsurePythonVersion(3, 12)
