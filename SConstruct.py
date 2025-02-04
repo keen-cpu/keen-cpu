8041c7ddf7642f5d6c18dc5c6af6323b3a6aa506
Jacob Koziej <jacobkoziej@gmail.com>
2025-02-04 16:48:50 -0500

    Add variable to enable verbose output

--- SConstruct.py
+++ SConstruct.py
@@ -1,19 +1,34 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 # SConstruct.py
-# Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
+# Copyright (C) 2024--2025  Jacob Koziej <jacobkoziej@gmail.com>
 
 import os
 
 from SCons.Environment import Environment
 from SCons.Script import (
+    ARGUMENTS,
     EnsurePythonVersion,
     EnsureSConsVersion,
+)
+from SCons.Variables import (
+    BoolVariable,
+    Variables,
 )
 
 EnsureSConsVersion(4, 7, 0)
 EnsurePythonVersion(3, 12)
 
+
+vars = Variables("overrides.py", ARGUMENTS)
+
+vars.AddVariables(
+    BoolVariable(
+        "verbose",
+        help="enable verbose output",
+        default=False,
+    ),
+)
 
 env = Environment(
     ENV={
@@ -25,4 +40,5 @@ env = Environment(
         "default",
         "Verilate",
     ],
+    variables=vars,
 )
