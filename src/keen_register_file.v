53bc7900a19321152d58222b403ad390701f50e3
Jacob Koziej <jacobkoziej@gmail.com>
2025-02-17 16:49:23 -0500

    Use `XLEN` to stay consistent with ISA manual

--- src/keen_register_file.v
+++ src/keen_register_file.v
@@ -5,21 +5,21 @@
 
 module keen_register_file #(
   parameter integer REGISTERS = 32,
-  parameter integer WORD_SIZE = 32,
+  parameter integer XLEN      = 32,
   parameter integer READS     = 2,
   parameter integer WRITES    = 1,
 
-  localparam integer ADDRESS_SIZE = $clog2(WORD_SIZE)
+  localparam integer ADDRESS_SIZE = $clog2(XLEN)
 ) (
   input wire clk,
   input wire reset,
 
   input wire [ADDRESS_SIZE - 1:0] read_addresses [0:READS  - 1],
   input wire [ADDRESS_SIZE - 1:0] write_addresses[0:WRITES - 1],
-  input wire [WORD_SIZE    - 1:0] write_data     [0:WRITES - 1],
+  input wire [XLEN         - 1:0] write_data     [0:WRITES - 1],
   input wire                      write_enables  [0:WRITES - 1],
 
-  output reg [WORD_SIZE - 1:0] read_data[0:READS - 1]
+  output reg [XLEN - 1:0] read_data[0:READS - 1]
 );
   wire reset_clk;
   wire read_clk;
@@ -27,7 +27,7 @@ module keen_register_file #(
   assign reset_clk = clk & reset;
   assign read_clk  = clk & !reset;
 
-  reg [WORD_SIZE - 1:0] registers[0:REGISTERS - 1];
+  reg [XLEN - 1:0] registers[0:REGISTERS - 1];
 
   genvar i;
 
@@ -52,6 +52,6 @@ module keen_register_file #(
     assign write_enable  = write_enables[i];
 
     always @(posedge write_clk or posedge reset_clk)
-      registers[write_address] <= !reset ? write_data[i] : {WORD_SIZE{1'b0}};
+      registers[write_address] <= !reset ? write_data[i] : {XLEN{1'b0}};
   end
 endmodule
