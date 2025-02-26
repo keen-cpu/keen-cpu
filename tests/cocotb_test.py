4b2634a3285adf203c6eb63a9b758f4ac06b4246
Jacob Koziej <jacobkoziej@gmail.com>
2025-02-25 19:44:27 -0500

    Add `cocotb_test.py`

---
+++ tests/cocotb_test.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: MPL-2.0
+#
+# cocotb_test.py -- cocotb test config
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+from importlib import import_module
+from pathlib import Path
+from typing import Final
+
+from cocotb.runner import get_runner
+
+PROJECT_ROOT: Final[Path] = (Path(__file__).resolve().parent / "..").resolve()
+
+SIM: Final[str] = "icarus"
+
+BUILD: Final[Path] = PROJECT_ROOT / "build"
+INCLUDE: Final[Path] = PROJECT_ROOT / "include"
+SRC: Final[Path] = PROJECT_ROOT / "src"
+TESTS: Final[Path] = PROJECT_ROOT / "tests"
+
+SOURCES: Final[Path] = list(SRC.glob("**/*.v"))
+
+
+def runner(module_path: Path, test_module: str) -> None:
+    runner = get_runner(SIM)
+
+    build_dir = BUILD / "tests" / module_path.with_suffix(".d")
+    includes = [INCLUDE]
+    hdl_toplevel = module_path.stem
+
+    runner.build(
+        always=True,
+        build_dir=build_dir,
+        hdl_toplevel=hdl_toplevel,
+        includes=includes,
+        sources=SOURCES,
+    )
+
+    runner.test(
+        hdl_toplevel=hdl_toplevel,
+        test_module=test_module,
+    )
+
+
+_module = import_module(__name__)
+
+for module_path in TESTS.glob("**/keen_*.py"):
+    module_path = module_path.relative_to(TESTS)
+
+    test_module = module_path.stem
+
+    def module_runner() -> None:
+        runner(module_path, test_module)
+
+    setattr(_module, f"test_{test_module}", module_runner)
