d6e2308b11eb035a8336e555d3254b2f7b75c3a7
Jacob Koziej <jacobkoziej@gmail.com>
2025-02-26 01:56:50 -0500

    Define `ClockedModule`

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MPL-2.0
+#
+# __init__.py -- models
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+from . import (
+    abc,
+)
+
+__all__ = [
+    "abc",
+]
